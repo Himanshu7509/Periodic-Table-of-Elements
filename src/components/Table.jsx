d5d5def95d1a35cae526943eb5272d0751e7a95e
HIMANSHU DIWAKAR THAKRE <himanshuthakre7509@gmail.com>
2026-05-30 16:02:45 +0530

    table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,6 +3,15 @@ import data from "../components/data/data.json";
 import { useState, createContext, useContext } from "react";
 import { ThemeContext } from "../App";
 import Modal from "./Modal";
+import Actinides from "./rows/Actinides";
+import SeventhhRow from "./rows/SeventhRow";
+import FirstRow from "./rows/FirstRow";
+import SecondRow from "./rows/SecondRow";
+import ThirdRow from "./rows/ThirdRow";
+import FourthRow from "./rows/FourthRow";
+import FifthRow from "./rows/FifthRow";
+import SixthRow from "./rows/SixthRow";
+import Lanthanides from "./rows/Lanthanides";
 
 const ModalContext = createContext();
 
@@ -22,8 +31,51 @@ export default function Table() {
     <ModalContext.Provider value={toggleModal}>
       <div className="py-4 px-4">
         <Modal open={open} toggleModal={toggleModal} modalData={modalData} />
-        
 
+        <h2 className={`md:hidden py-2 text-${groupColor} font-semibold`}>
+          Group One
+        </h2>
+        <FirstRow data={data.elements} />
+
+        <h2 className={`md:hidden py-2 text-${groupColor} font-semibold`}>
+          Group Two
+        </h2>
+        <SecondRow data={data.elements} />
+
+        <h2 className={`md:hidden py-2 text-${groupColor} font-semibold`}>
+          Group Three
+        </h2>
+        <ThirdRow data={data.elements} />
+
+        <h2 className={`md:hidden py-2 text-${groupColor} font-semibold`}>
+          Group Four
+        </h2>
+        <FourthRow data={data.elements} />
+
+        <h2 className={`md:hidden py-2 text-${groupColor} font-semibold`}>
+          Group Five
+        </h2>
+        <FifthRow data={data.elements} />
+
+        <h2 className={`md:hidden py-2 text-${groupColor} font-semibold`}>
+          Group Six
+        </h2>
+        <SixthRow data={data.elements} />
+
+        <h2 className={`md:hidden py-2 text-${groupColor} font-semibold`}>
+          Group Seven
+        </h2>
+        <SeventhhRow data={data.elements} />
+
+        <h2 className={`md:hidden py-2 text-${groupColor} font-semibold`}>
+          Lanthanides
+        </h2>
+        <Lanthanides data={data.elements} />
+
+        <h2 className={`md:hidden py-2 text-${groupColor} font-semibold`}>
+          Actinides
+        </h2>
+        <Actinides data={data.elements} />
       </div>
     </ModalContext.Provider>
   );
