29b46df8e1bf2de2c5f8277effb18f8d144947ce
HIMANSHU DIWAKAR THAKRE <himanshuthakre7509@gmail.com>
2026-05-30 15:42:37 +0530

    modalContext

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import data from "../components/data/data.json";
+import { useState, createContext, useContext } from "react";
+import { ThemeContext } from "../App";
+import Modal from "./Modal";
 
-const Table = () => {
+const ModalContext = createContext();
+
+export default function Table() {
+  const [open, setOpen] = useState(false);
+  const [modalData, setModalData] = useState("");
+
+  const themeContext = useContext(ThemeContext);
+  let groupColor = themeContext ? "white" : "#1e293b";
+
+  function toggleModal(element) {
+    setModalData(element);
+    setOpen((prevState) => !prevState);
+  }
+
   return (
-    <div>Table</div>
-  )
+    <ModalContext.Provider value={toggleModal}>
+      <div className="py-4 px-4">
+        <Modal open={open} toggleModal={toggleModal} modalData={modalData} />
+        table
+      </div>
+    </ModalContext.Provider>
+  );
 }
-
-export default Table
+export { ModalContext };
